54913a19656c993cdea7dc65c7a478ad6f32b0d7
Ankit Pandit <58819767+ankit1215@users.noreply.github.com>
2025-12-05 16:44:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function makeid(l) {
-  // write your code here
+  l = Number(l); // convert prompt input to number
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+
+  for (let i = 0; i < l; i++) {
+    result += chars[Math.floor(Math.random() * chars.length)];
+  }
+
+  return result;
 }
 
 // Do not change the code below.
